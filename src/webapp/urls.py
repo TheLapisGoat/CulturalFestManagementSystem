94d75c5271132e05e7930ee2698b4440dc9e8fc5
SAMPREETH-R-S <sampreeth2003@gmail.com>
2024-02-28 22:24:29 +0530

    organizer

--- src/webapp/urls.py
+++ src/webapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("organizer", views.organiser_view, name='organiser'),
 ]
--- src/webapp/views.py
+++ src/webapp/views.py
@@ -10,8 +10,17 @@ def index(request):
     return HttpResponse("Hello, world. You're at the webapp index.")
 
 
-@login_required(login_url='main-login')
+#@login_required(login_url='main-login')
 def organiser_view(request):
+    if(request.user.is_anonymous):
+        return HttpResponse("You're not logged in")
     if(request.user.role!='organizer'):
         return redirect("index")
-    event_list = Organized_by.objects.filter(organizer_id=request.user
+    event_list = Organized_by.objects.filter(organizer_id=request.user)
+    event_id_list = event_list.values_list('event_id',flat=True)
+    events = Event.objects.all()
+    res = []
+    for event in events:
+        if event.event_id in event_id_list:
+            res.append(event)
+    return render(request, 'organiser_view.html', {'events': res})
