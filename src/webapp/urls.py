9bf9e0f94ee337b8ab0bb00a14abc1d3aef367a0
thelapisgoat <sourodeepdatta@gmail.com>
2024-02-29 15:35:14 +0530

    Minor change

--- src/webapp/urls.py
+++ src/webapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path("organizer", views.organiser_view, name='organiser'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('register/', views.RegisterView.as_view(), name='register'),
