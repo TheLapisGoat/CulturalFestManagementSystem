e0407a08ea2f9de8810bd696ea09e03fb456c779
ashwinpra <ashprasanth2003@gmail.com>
2024-03-01 15:27:56 +0530

    readded volunteer views

--- src/webapp/urls.py
+++ src/webapp/urls.py
@@ -6,14 +6,21 @@ urlpatterns = [
     path('', views.MainViewRedirect.as_view(), name='index-redirector'),
     path("admin/", admin.site.urls),
     path("admin/", admin.site.index, name = "admin-home"),
+
     path("organizer/event/<int:event_id>", views.OrganizerEventView.as_view(), name='organizer-event'),
     path('organizer/profile/', views.OrganizerProfileView.as_view(), name='organizer-profile'),
-    path("organizer", views.OrganizerHomeView.as_view(), name='organizer-home'),
+    path("organizer/", views.OrganizerHomeView.as_view(), name='organizer-home'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('otp-verification/', views.OTPVerificationView.as_view(), name='otp-verification'),
 
     path('index/', views.HomeView.as_view(), name='Home'),
+
+    path('student/', views.student_home_view, name='student-home'),
+    path('student/volunteer/', views.StudentVolunteerView.as_view(), name='student-volunteer'),
+    path('student/register_volunteer/', views.StudentRegisterVolunteerView.as_view(), name='student-register-volunteer'),
+    path('student/profile/', views.student_profile_view, name='student-profile'),
+
 
 
     path('events/', views.events, name='events'),
