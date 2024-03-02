973f9ee1ff00508126760b1edb38b4e6718986d0
Sampreeth R S <sampreeth2003@gmail.com>
2024-03-02 19:42:42 +0530

    added register as volunteer and register as participant for student

--- src/webapp/models.py
+++ src/webapp/models.py
@@ -156,7 +156,7 @@ class Participant_Accomodation(models.Model):
     room_number = models.CharField(max_length=50,blank=False)
 
 class StudentEvent(models.Model):
-    student = models.ForeignKey(User_Entity, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     # Add other fields as needed
     class Meta:
